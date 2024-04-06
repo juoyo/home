13ba4eab28b9e54a484bd034941b0c956bb2988b
juoyo <juoyohub@gmail.com>
2024-04-07 00:09:33 +0800

    ''

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start cmd /k "git add . && set now=%date% %time%&& git commit -m '%now%' && git push && echo 'complete!' && pause"
+start cmd /k "git add . && set now=%date% %time% && git commit -m '%now%' && git push && echo 'complete!' && pause"
 
